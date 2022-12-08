b2bca89acd334dfdccbdf73363b7e1298832eba0
mikicode21 <118551402+mikicode21@users.noreply.github.com>
2022-12-09 01:08:32 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - Prints "and that piece of art is useful" - - Dora Korpar
+ * followed by a new line, to standard error.
+ * Return: 1 always
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
 }
