a7c85f501852a253c3a907112eef215256555369
mikicode21 <mikidegef@gmail.com>
2022-12-08 13:07:39 -0800

    k

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
-
-int main()
+/**                                                                                                                                                                                            *
+ * main -print line using printf                                                                                                                                                          
+ * 
+ * Return: 0                                                                                                                                                                                *
+*/
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,")
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
