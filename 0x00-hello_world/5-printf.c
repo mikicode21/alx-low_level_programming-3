789806c07e78df7529217dd8b59346f7b6c69c38
mikicode21 <118551402+mikicode21@users.noreply.github.com>
2022-12-09 01:13:21 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
-#include<stdio.h>
-/**                                                                                                                                                                                            *
- * main -print line using printf                                                                                                                                                          
- * 
- * Return: 0                                                                                                                                                                                *
-*/
+#include <stdio.h>
 
+/**
+ * Description: main - prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
