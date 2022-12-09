2cf91e0c9254ebe712e09bb5c3f722dfb986da95
mikicode21 <118551402+mikicode21@users.noreply.github.com>
2022-12-10 01:40:23 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 #include <stdio.h>
 #include <unistd.h>
 /**
@@ -12,4 +10,3 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
         return (1);
 }
->>>>>>> d15365ce1b5fb86f3e034883fc03ad43e786348e
