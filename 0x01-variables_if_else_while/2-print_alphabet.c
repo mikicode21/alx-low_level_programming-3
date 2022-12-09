56b629f417d8770607e52139fdc3b65656c8663e
mikicode21 <118551402+mikicode21@users.noreply.github.com>
2022-12-10 02:58:11 +0300

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,65 +1,23 @@
-nclude <stdlib.h>
-
+#include <stdlib.h>
 #include <time.h>
-
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - print if the number is positive, zero, or negative
- *
- *   *
- *
- *    * Description: using the main function
- *
- *     * this program prints "programming is positive, zero, or negative
- *
- *      * Return: 0
- *
- *       */
-
-
+ * main - print if the number is positive, zero, or negative
+ * Description: using the main function
+ * this program prints "programming is positive, zero, or negative
+ * Return: 0 always
+ */
 
 int main(void)
-
 {
+	char ch;
 
-	int n;
-
-
-
-	srand(time(0));
-
-	n = rand() - RAND_MAX / 2;
-
-	/* your code goes there*/
-
-	if (n > 0)
+	for (ch = 'a' ; ch <= 'z' ; ch++)
 
 	{
-
-		printf("%i is positive\n", n);
-
+		putchar(ch);
 	}
-
-	else if (n == 0)
-
-	{
-
-		printf("%i is zero\n", n);
-
-	}
-
-	else if (n < 0)
-
-	{
-
-		printf("%i is negative\n", n);
-
-	}
-
+	putchar('\n');
 	return (0);
-
 }
