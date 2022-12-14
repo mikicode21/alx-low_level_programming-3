9839491c516a61721f0d406821f18cd4706e337b
mikicode21 <118551402+mikicode21@users.noreply.github.com>
2022-12-14 03:46:09 +0300

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,15 @@
-i
+#include "main.h"
+/**
+ * add - adds two integrers and returns the result
+ *
+ * @x: number being added
+ *
+ * @y: number being added
+ *
+ * Return: Always 0.
+ *
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
